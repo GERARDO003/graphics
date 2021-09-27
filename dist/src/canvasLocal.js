650c9f811d5eb73ff07fb81021a2dfab0fcb8f99
GeRa <luisvalenciajosegerardo75@gmail.com>
2021-09-26 23:32:04 -0500

    figura concentrica

--- dist/src/canvasLocal.js
+++ dist/src/canvasLocal.js
@@ -8,8 +8,8 @@ export class CanvasLocal {
      protected centerX: number;
      protected centerY: number;*/
     constructor(g, canvas) {
-        this.rWidth = 3;
-        this.rHeight = 3;
+        this.rWidth = 1.5;
+        this.rHeight = 2;
         this.rWidth2 = 2.5;
         this.rHeight2 = 2.5;
         //this.rWidth = 6;
@@ -34,9 +34,73 @@ export class CanvasLocal {
       return Math.sin(x*2.5);
     }*/
     paint() {
-        this.drawLine(this.iX(0), this.iY(0), this.iX(1), this.iY(0));
+        /*this.drawLine(this.iX(0), this.iY(0), this.iX(1), this.iY(0));
         this.drawLine(this.iX(1), this.iY(0), this.iX(1), this.iY(1));
         this.drawLine(this.iX(1), this.iY(1), this.iX(0), this.iY(1));
-        this.drawLine(this.iX(0), this.iY(1), this.iX(0), this.iY(0));
+        this.drawLine(this.iX(0), this.iY(1), this.iX(0), this.iY(0));*/
+        let side = 0.7 * 480;
+        let sideHalf = 0.5 * side;
+        let xCenter = 320;
+        let yCenter = 240;
+        let h = sideHalf * Math.sqrt(3);
+        let xA, yA, xB, yB, xC, yC, xA1, yA1, xB1, yB1, xC1, yC1, p, q;
+        q = 0.03;
+        p = 1 - q;
+        xA = xCenter - sideHalf;
+        yA = yCenter - 0.5 * h;
+        xB = xCenter + sideHalf;
+        yB = yA;
+        xC = xCenter;
+        yC = yCenter + 0.5 * h;
+        for (let i = 0; i < 50; i++) {
+            this.graphics.strokeStyle = 'blue';
+            this.drawLine(xA, yA, xB, yB);
+            this.drawLine(xB, yB, xC, yC);
+            this.drawLine(xC, yC, xA, yA);
+            xA1 = p * xA + q * xB;
+            yA1 = p * yA + q * yB;
+            xB1 = p * xB + q * xC;
+            yB1 = p * yB + q * yC;
+            xC1 = p * xC + q * xA;
+            yC1 = p * yC + q * yA;
+            xA = xA1;
+            xB = xB1;
+            xC = xC1;
+            yA = yA1;
+            yB = yB1;
+            yC = yC1;
+        }
+        for (let i = 0; i < 50; i++) {
+            let side = 0.7 * 580;
+            let sideHalf = 0.5 * side;
+            let xCenter = 320;
+            let yCenter = 240;
+            let h = sideHalf * Math.sqrt(3);
+            let xA, yA, xB, yB, xC, yC, xA1, yA1, xB1, yB1, xC1, yC1, p, q;
+            q = 0.15;
+            p = 1 - q;
+            xA = xCenter - sideHalf;
+            yA = yCenter - 0.5 * h;
+            xB = xCenter + sideHalf;
+            yB = yA;
+            xC = xCenter;
+            yC = yCenter + 0.5 * h;
+            this.graphics.strokeStyle = 'black';
+            this.drawLine(xB, yB, xC, yC);
+            this.drawLine(xC, yC, xA, yA);
+            this.drawLine(xA, yA, xB, yB);
+            xA1 = p * xA + q * xB;
+            yA1 = p * yA + q * yB;
+            xB1 = p * xB + q * xC;
+            yB1 = p * yB + q * yC;
+            xC1 = p * xC + q * xA;
+            yC1 = p * yC + q * yA;
+            xA = xA1;
+            xB = xB1;
+            xC = xC1;
+            yA = yA1;
+            yB = yB1;
+            yC = yC1;
+        }
     }
 }
--- src/canvasLocal.ts
+++ src/canvasLocal.ts
@@ -2,8 +2,8 @@
 export class CanvasLocal {
   //atributos
   protected graphics: CanvasRenderingContext2D;
-  rWidth: number =3;
-  rHeight: number = 3;
+  rWidth: number =1.5;
+  rHeight: number = 2;
   maxX: number;
   maxY: number;
   pixelSize: number;
@@ -50,14 +50,85 @@ export class CanvasLocal {
   }*/
 
 
-  paint() {
+  paint() { 
     
-    this.drawLine(this.iX(0), this.iY(0), this.iX(1), this.iY(0));
+    /*this.drawLine(this.iX(0), this.iY(0), this.iX(1), this.iY(0));
     this.drawLine(this.iX(1), this.iY(0), this.iX(1), this.iY(1));
     this.drawLine(this.iX(1), this.iY(1), this.iX(0), this.iY(1));
-    this.drawLine(this.iX(0), this.iY(1), this.iX(0), this.iY(0));
+    this.drawLine(this.iX(0), this.iY(1), this.iX(0), this.iY(0));*/
+    
+  let side= 0.7 * 480; 
+  let sideHalf= 0.5 * side;
+  let xCenter= 320;
+  let yCenter= 240;
+
+  let h= sideHalf * Math.sqrt(3);
+  let xA, yA, xB, yB, xC, yC, xA1, yA1, xB1, yB1, xC1, yC1, p, q;
+  q= 0.03;
+  p= 1-q;
+  xA= xCenter - sideHalf;
+  yA=yCenter -0.5 * h;
+  xB= xCenter + sideHalf;
+  yB=yA;
+  xC= xCenter;
+  yC=yCenter + 0.5 * h;
+
+  for (let i=0;i<50;i++)
+  {
+    this.graphics.strokeStyle = 'blue';
+    this.drawLine(xA, yA, xB,  yB);
+    this.drawLine(xB, yB, xC,  yC);
+    this.drawLine(xC, yC, xA,  yA);
+
+    xA1 = p * xA + q * xB;
+    yA1 = p * yA + q * yB;
+    xB1 = p * xB + q * xC;
+    yB1 = p * yB + q * yC;
+    xC1 = p * xC + q * xA;
+    yC1 = p * yC + q * yA;
+    xA = xA1; xB = xB1; xC = xC1;
+    yA = yA1; yB = yB1; yC = yC1;
+
   }
+
+  for (let i=0;i<50;i++)
+  {
+    let side= 0.7 * 580; 
+  let sideHalf= 0.5 * side;
+  let xCenter= 320;
+  let yCenter= 240;
+   
+  let h= sideHalf * Math.sqrt(3);
+  let xA, yA, xB, yB, xC, yC, xA1, yA1, xB1, yB1, xC1, yC1, p, q;
+  q= 0.15;
+  p= 1-q;
+  xA= xCenter - sideHalf;
+  yA=yCenter -0.5 * h;
+  xB= xCenter + sideHalf;
+  yB=yA;
+  xC= xCenter;
+  yC=yCenter + 0.5 * h;
+
   
+
+    this.graphics.strokeStyle = 'black';
+    this.drawLine(xB, yB, xC,  yC);
+    this.drawLine(xC, yC, xA,  yA);
+    this.drawLine(xA, yA, xB,  yB);
+
+    xA1 = p * xA + q * xB;
+    yA1 = p * yA + q * yB;
+    xB1 = p * xB + q * xC;
+    yB1 = p * yB + q * yC;
+    xC1 = p * xC + q * xA;
+    yC1 = p * yC + q * yA;
+    xA = xA1; xB = xB1; xC = xC1;
+    yA = yA1; yB = yB1; yC = yC1;
+
+  }
+
+  }
+
     //dibuja la cuadricula
     /*this.graphics.strokeStyle = 'lightgray';
     for (let x = -3; x <= 3; x+=0.25){
